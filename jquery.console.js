22ffefcc6cdde6afa5441d77755aace104b6ec40
Sam Sehnert <sam@teamdf.com>
2012-07-04 21:20:04 +1200

    Fixed an issue where chaining was broken when console was disabled.

--- jquery.console.js
+++ jquery.console.js
@@ -30,18 +30,16 @@
 		if( _console && _console.log ){
 			try{
 				_console.log.apply( _console, arguments );
-				return  $(this).each(function(){ _console.log.apply( _console, arguments ); });
+				this.each(function(){ _console.log.apply( _console, arguments ); });
 			} catch(e){
 				try{
 					// Hack for IE, which doesn't allow .apply on console methods.
 					for( var i in arguments ){ _console.log( arguments[i] ); }
-					return $(this).each(function(i,elem){ _console.log( i, elem ); })
-				} catch(e){
-					// Just in case...
-					return $(this);
-				}
+					this.each(function(i,elem){ _console.log( i, elem ); })
+				} catch(e){ /* Just in case... */}
 			}
 		}
+		return this;
 	}
 	
 	// The methods array will allow you to define public methods that
